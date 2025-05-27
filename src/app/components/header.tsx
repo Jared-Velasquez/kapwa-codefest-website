c094192fa358529de0cb374bad4cc6d4e0d5bd23
Kieran Llarena <baymax709@gmail.com>
2025-05-27 14:09:51 -0400

    chore: comment out unused import and accidentally formatted the file w/my prettier

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,19 +1,22 @@
-"use client"
-import styled from 'styled-components';
+"use client";
+// import styled from 'styled-components';
 
-
-export default function Header(){
-    return(
-        <header className="text-center text-black z-10 p-5">
-            <h1 className="font-[TDSulog] text-[3rem]">
-                Filipino-American
-            </h1>
-            <h2 className="flex font-[Maragsa] text-[2rem] justify-between items-center">
-                <img className="rotate-90" src="/icons/TabArrow.png" alt="Arrow icon" />
-                <div className="flex justify-between items-center text-black py-[1vh] px-[2vw]">History Month</div>
-                <img className="rotate-270" src="/icons/TabArrow.png" alt="Arrow icon" />
-            </h2>
-            <h1 className="font-[TDSulog] text-[3rem]">Hackathon</h1>
-        </header>
-    )
+export default function Header() {
+  return (
+    <header className="text-center text-black z-10 p-5">
+      <h1 className="font-[TDSulog] text-[3rem]">Filipino-American</h1>
+      <h2 className="flex font-[Maragsa] text-[2rem] justify-between items-center">
+        <img className="rotate-90" src="/icons/TabArrow.png" alt="Arrow icon" />
+        <div className="flex justify-between items-center text-black py-[1vh] px-[2vw]">
+          History Month
+        </div>
+        <img
+          className="rotate-270"
+          src="/icons/TabArrow.png"
+          alt="Arrow icon"
+        />
+      </h2>
+      <h1 className="font-[TDSulog] text-[3rem]">Hackathon</h1>
+    </header>
+  );
 }
