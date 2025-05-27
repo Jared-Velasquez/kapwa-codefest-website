f1a493bce866ac3be2ece1310a4b28ae2ff1c87a
Kieran Llarena <baymax709@gmail.com>
2025-05-27 16:49:43 -0400

    feat: displaying schools who are organizing the hackathon

--- src/app/sections/Schools.tsx
+++ src/app/sections/Schools.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function Schools() {
   return (
     <div className="flex items-center flex-col">
@@ -10,6 +12,50 @@ export default function Schools() {
           <strong>6 schools</strong> nationwide, take a look at who&apos;s
           involved!
         </h3>
+      </div>
+      <div className="pt-12.5 md:pt-30 xl:pt-55 px-5 flex items-center justify-center flex-wrap gap-20">
+        <Image
+          alt="Boston University is a school helping organize this hackathon"
+          src="/schools/bu.png"
+          width={200}
+          height={150}
+          className="object-contain"
+        />
+        <Image
+          alt="Wayne State University is a school helping organize this hackathon"
+          src="/schools/wsu.png"
+          width={200}
+          height={150}
+          className="object-contain"
+        />
+        <Image
+          alt="University of Michigan is a school helping organize this hackathon"
+          src="/schools/m-1.png"
+          width={200}
+          height={150}
+          className="object-contain"
+        />
+        <Image
+          alt="University of California, Irvine is a school helping organize this hackathon"
+          src="/schools/uci.webp"
+          width={200}
+          height={150}
+          className="object-contain"
+        />
+        <Image
+          alt="University of California, San Diego is a school helping organize this hackathon"
+          src="/schools/ucsd.png"
+          width={200}
+          height={150}
+          className="object-contain"
+        />
+        <Image
+          alt="University of California, Los Angeles is a school helping organize this hackathon"
+          src="/schools/ucla.png"
+          width={200}
+          height={150}
+          className="object-contain"
+        />
       </div>
     </div>
   );
