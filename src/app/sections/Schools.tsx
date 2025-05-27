f65ceee4008cf2c6d6bc1edb477c051b8d817813
Kieran Llarena <baymax709@gmail.com>
2025-05-27 16:10:38 -0400

    chore: changed the participating schools and clubs section description

--- src/app/sections/Schools.tsx
+++ src/app/sections/Schools.tsx
@@ -1,11 +1,16 @@
 export default function Schools() {
-    return(
-        <div className="flex items-center flex-col">
-            <div className="self-center xl:self-end xl:pr-[15vw] pt-[20vh] w-[80vw] xl:w-[37vw]">
-                <h1 className="text-[3rem] text-center font-[Maragsa] text-black py-[5vh]">Participating Schools and Clubs</h1>
-                <h3 className="text-black text-[calc(2px+2vh)] font-normal font-[instrument sans] text-center">We are honored to be hosting this hackathon with over x schools and x clubs nationwide. Take a look at our participants!</h3>
-            </div>
-        </div>
-    )
-
+  return (
+    <div className="flex items-center flex-col">
+      <div className="self-center xl:self-end xl:pr-[15vw] pt-[20vh] w-[80vw] xl:w-[37vw]">
+        <h1 className="text-[3rem] text-center font-[Maragsa] text-black py-[5vh]">
+          Participating Schools
+        </h1>
+        <h3 className="text-black text-[calc(2px+2vh)] font-normal font-[instrument sans] text-center">
+          We are honored to be hosting this hackathon with{" "}
+          <strong>6 schools</strong> nationwide, take a look at who&apos;s
+          involved!
+        </h3>
+      </div>
+    </div>
+  );
 }
