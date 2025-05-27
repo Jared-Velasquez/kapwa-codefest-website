a261020e19f9f8ac737ac931972d48b3d34b7aaf
Kieran Llarena <baymax709@gmail.com>
2025-05-27 14:58:58 -0400

    chore: Event -> EventCard

--- src/app/sections/Itinerary.tsx
+++ src/app/sections/Itinerary.tsx
@@ -1,4 +1,4 @@
-import Event from "@/app/components/ItineraryComponents/EventCard";
+import EventCard from "@/app/components/ItineraryComponents/EventCard";
 import DayCard from "@/app/components/ItineraryComponents/DayCard";
 
 export default function Itinerary() {
@@ -11,16 +11,16 @@ export default function Itinerary() {
       {/* First Day */}
       <div className="flex flex-col justify-center gap-10">
         <DayCard date="Friday, October 3rd">
-          <Event
+          <EventCard
             time="6 PM EST/ 3 PM PST"
             eventName="How to construct a Product Requirements Document"
           />
-          <Event
+          <EventCard
             time="7 PM EST / 4 PM PST"
             eventName="Midwest / East Coast Business Session"
             description="A chance to talk to the businesses!"
           />
-          <Event
+          <EventCard
             time="8 PM EST / 5 PM PST"
             eventName="West Coast Business Session"
             description="A chance to talk to the businesses!"
@@ -30,39 +30,39 @@ export default function Itinerary() {
 
         {/* Second Day */}
         <DayCard date="Saturday, October 4th">
-          <Event
+          <EventCard
             time="11 AM EST / 8 AM PST"
             eventName="Midwest / East Coast Business Session (2 or 3)"
           />
-          <Event
+          <EventCard
             time="1 PM EST / 11 AM PST"
             eventName="West Coast Business Session (2 or 3)"
           />
-          <Event
+          <EventCard
             time="3 PM EST / 12 PM PST"
             eventName="Devpost Draft Submissions Due"
           />
-          <Event
+          <EventCard
             time="4-5 PM EST / 1-2 PM PST"
             eventName="Workshop"
             description="Details TBD"
           />
-          <Event
+          <EventCard
             time="5-6 PM EST / 2-3 PM PST"
             eventName="Workshop"
             description="Details TBD"
           />
-          <Event
+          <EventCard
             time="6-7 PM EST / 3-4 PM PST"
             eventName="Workshop"
             description="Details TBD"
           />
-          <Event
+          <EventCard
             time="7-8 PM EST / 4-5 PM PST"
             eventName="Workshop"
             description="Details TBD"
           />
-          <Event
+          <EventCard
             time="8-9 PM EST / 5-6 PM PST"
             eventName="Workshop"
             description="Details TBD"
@@ -71,16 +71,16 @@ export default function Itinerary() {
 
         {/* Third Day */}
         <DayCard date="Sunday, October 5th">
-          <Event
+          <EventCard
             time="1 PM EST / 11 AM PST"
             eventName="How to Sell Your Idea / Solution Workshop"
           />
-          <Event time="3 PM EST / 12 PM PST" eventName="Hacking Ends" />
-          <Event
+          <EventCard time="3 PM EST / 12 PM PST" eventName="Hacking Ends" />
+          <EventCard
             time="3:30 PM EST / 12:30 PM PST"
             eventName="Devpost Final Submission is Due"
           />
-          <Event
+          <EventCard
             time="5:30-7:30 PM EST / 2:30-4:30 PM PST"
             eventName="Career Panel on How The Panelists Broke into Tech"
           />
@@ -88,7 +88,7 @@ export default function Itinerary() {
 
         {/* Last Day */}
         <DayCard date="Monday, October 6th">
-          <Event time="11 AM EST" eventName="Winners Anounced" />
+          <EventCard time="11 AM EST" eventName="Winners Anounced" />
         </DayCard>
       </div>
     </div>
