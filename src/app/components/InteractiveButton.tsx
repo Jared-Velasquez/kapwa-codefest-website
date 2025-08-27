754609ea6fb6b0e7e6681dd28cc7ad9fc7eab035
Jared Velasquez <jaredvel25@ucla.edu>
2025-08-27 00:02:20 -0700

    feat: update interactive button padding on mobile

--- src/app/components/InteractiveButton.tsx
+++ src/app/components/InteractiveButton.tsx
@@ -4,7 +4,7 @@ export default function InteractiveButton({text, isActive = true, onClick}: {tex
     return(
         <button
             onClick={onClick}
-            className="bg-gradient-to-r from-[#e9a400] to-[#f9d46c] rounded-[40px] px-[2vw] py-[2vh] text-black cursor-pointer shadow-[0_6px_12px_rgba(0,0,0,0.25)] transition-transform duration-200 font-instrument-sans font-medium flex items-center justify-center no-underline w-fit z-10 hover:scale-[1.05] active:scale-[0.95]"
+            className="bg-gradient-to-r from-[#e9a400] to-[#f9d46c] rounded-[40px] md:px-[2vw] md:py-[2vh] px-[4vw] py-[2.5vh] text-black cursor-pointer shadow-[0_6px_12px_rgba(0,0,0,0.25)] transition-transform duration-200 font-instrument-sans font-medium flex items-center justify-center no-underline w-fit z-10 hover:scale-[1.05] active:scale-[0.95]"
             style={{ opacity: isActive ? 1 : 0.5, pointerEvents: isActive ? 'auto' : 'none', color: isActive ? 'black' : 'gray' }}
         >
 
