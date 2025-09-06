3812af29494777061a0b49991b155d38f65355e1
alyssa najera <anajera@bu.edu>
2025-09-06 18:11:43 -0400

    added instagram and email to footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,8 +1,21 @@
+"use client";
+
+import Image from "next/image";
 
 export default function Footer() {
-    return(
-        <footer className="text-center">
-            <p className="text-black text-[calc(10px+3vh)] font-[550] font-[Maragsa] pb-[2vh]">Salamat!</p>
+    return (
+        <footer className="text-center pb-[2vh]">
+            <p className="text-black text-[calc(10px+3vh)] font-[550] font-[Maragsa]">
+                Salamat!
+            </p>
+            <div className={"flex justify-center items-center gap-5"}>
+                <a href="mailto:kapwacodefest@gmail.com" rel="noopener noreferrer">
+                    <Image src="/icons/email.svg" alt="email" width={30} height={30} className="cursor-pointer"/>
+                </a>
+                <a href="https://www.instagram.com/kapwacodefest" rel="noopener noreferrer">
+                    <Image src="/icons/instagram.svg" alt="email" width={20} height={20} className="cursor-pointer invert"/>
+                </a>
+            </div>
         </footer>
-    )
+    );
 }
