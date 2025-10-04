155f1ac3bb55082cfa72614199ebc9f0d0c96ac3
Jared-Velasquez <107715384+Jared-Velasquez@users.noreply.github.com>
2025-10-03 18:21:31 -0700

    fix: remove unused vars on LandingPage

--- src/app/sections/LandingPage.tsx
+++ src/app/sections/LandingPage.tsx
@@ -2,9 +2,7 @@ import axios from 'axios';
 import HeroHeader from '@/app/components/HeroHeader';
 import { useEffect, useMemo, useState } from 'react';
 import { useAuth } from 'react-oidc-context';
-import Image from 'next/image';
-import InteractiveButton from '../components/InteractiveButton';
-import { AnimatePresence, motion } from 'framer-motion';
+import { motion } from 'framer-motion';
 
 const MIN_DELAY_MS = 500;
 const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
@@ -15,14 +13,6 @@ export default function LandingPage() {
     const [isLoading, setIsLoading] = useState(true);
     const [isProfileComplete, setIsProfileComplete] = useState(false);
     const [isRegistered, setIsRegistered] = useState(false);
-
-    const uiState: UIState = useMemo(() => {
-        if (!auth.isAuthenticated) return 'doneOrGuest';
-        if (isLoading) return 'loading';
-        if (!isProfileComplete) return 'profileIncomplete';
-        if (isProfileComplete && !isRegistered) return 'canRegister';
-        return 'doneOrGuest';
-    }, [auth.isAuthenticated, isLoading, isProfileComplete, isRegistered]);
 
     useEffect(() => {
         if (
@@ -62,48 +52,17 @@ export default function LandingPage() {
         fetchRegistrationData();
     }, [auth.isAuthenticated, auth.user?.id_token, auth.user?.profile?.sub]);
 
-    // Redirect the user to the profile page via NextJS routing
-    function redirectToProfile() {
-        window.location.href = '/profile';
-    }
-
-    async function registerUserForHackathon() {
-        setIsLoading(true);
-        console.log('Registering user for hackathon...');
-
-        try {
-            await Promise.all([
-                axios.post(
-                    `${process.env.NEXT_PUBLIC_API_GATEWAY_INVOKE_URL}/users/register`,
-                    null, // No body is needed for this endpoint
-                    {
-                        headers: {
-                            Authorization: `Bearer ${auth.user?.id_token}`,
-                        },
-                    }
-                ),
-                sleep(MIN_DELAY_MS),
-            ]);
-            setIsRegistered(true);
-        } catch (error) {
-            console.error('Error registering user:', error);
-        } finally {
-            setIsLoading(false);
-        }
-
-        console.log('User registered successfully');
-    }
 
     return (
         <div className="h-[100vh] sm:h-[100vh] flex justify-center items-center flex-col">
             <HeroHeader />
 
             <motion.div
-            key="registrationClosed"
-            initial={{ opacity: 0, y: 6 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: -6 }}
-            className="mt-6 w-full flex flex-col items-center"
+                key="registrationClosed"
+                initial={{ opacity: 0, y: 6 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -6 }}
+                className="mt-6 w-full flex flex-col items-center"
             >
             <div className="text-center font-sans text-black px-10 bg-gradient-to-r from-gray-200 to-gray-100 rounded-2xl mx-5 shadow-sm hover:shadow-lg transition-shadow duration-300 flex flex-col items-center text-xl">
                 <div className="pt-6 pb-6">
